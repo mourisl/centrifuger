64cf1ad91013812137018d680038455494c26f3f
mourisl <mourisl@hotmail.com>
2023-05-18 11:38:22 -0400

    Preliminary implement processing taxonomy information

--- CentrifugerBuild.cpp
+++ CentrifugerBuild.cpp
@@ -8,17 +8,16 @@
 #include "compactds/FMBuilder.hpp"
 #include "Taxonomy.hpp"
 
-char nucToNum[26] = { 0, -1, 1, -1, -1, -1, 2, 
-	-1, -1, -1, -1, -1, -1, 0,
-	-1, -1, -1, -1, -1, 3,
-	-1, -1, -1, -1, -1, -1 } ;
+char usage[] = "./centrifuger-build [OPTIONS]:\n"
+  "";
 
 static const char *short_options = "f:o:t:" ;
 static struct option long_options[] = {
 			{ "bmax", required_argument, 0, ARGV_BMAX},
 			{ "dcv", required_argument, 0, ARGV_DCV},
       { "offrate", required_argument, 0, ARGV_OFFRATE},
-      { "convertion-table", required_argument, 0, ARGV_CONVERTION_TABLE},
+      { "taxonomy-tree", required_argument, 0, ARGV_TAXONOMY_TREE},
+      { "conversion-table", required_argument, 0, ARGV_CONVERSION_TABLE},
 			{ "name-table", required_argument, 0, ARGV_NAME_TABLE},
 			{ (char *)0, 0, 0, 0} 
 			} ;
@@ -33,7 +32,17 @@ int main(int argc, char *argv[])
   }
 	int c, option_index ;
 	option_index = 0 ;
+  char outputPrefix[1024] = "centrifuger" ;
+  char outputFileName[1024] ;
+  char *taxonomyFile = NULL ; // taxonomy tree file
+  char *nameTable = NULL ;
+  char *conversionTable = NULL ;
+  int threadCnt = 1 ;
+  ReadFiles genomeFile ;
+
   FMBuilder fmBuilder ;
+  Taxonomy taxonomy ;
+
   while (1)
   {
 		c = getopt_long( argc, argv, short_options, long_options, &option_index ) ;
@@ -41,22 +50,66 @@ int main(int argc, char *argv[])
 		if (c == -1)
 			break ;
   
-    if (c == 'f')
+    if (c == 'f') // genome fasta file
     {
-
+      genomeFile.AddReadFile(optarg, false) ;
     }
     else if (c == 'o')
     {
-
+      strcpy(outputPrefix, optarg) ;
     }
     else if (c == 't')
     {
-
+      threadCnt = atoi(optarg) ;
     }
+    else if (c == ARGV_TAXONOMY_TREE)
+    {
+      taxonomyFile = strdup(optarg) ;  
+    }
+    else if (c == ARGV_NAME_TABLE)
+    {
+      nameTable = strdup(optarg) ;
+    }
+    else if (c == ARGV_CONVERSION_TABLE)
+    {
+      conversionTable = strdup(optarg) ;
+    }
+		else
+		{
+			fprintf( stderr, "Unknonw parameter found\n%s", usage ) ;
+			return EXIT_FAILURE ;
+		}
   }
-  FixedSizeElemArray genomes ;
-  
+
+  if (!taxonomyFile)
+  {
+    fprintf(stderr, "Need to use --taxonomy-tree to specify the taxonomy tree.\n") ;
+    return EXIT_FAILURE ;
+  }
+  if (!nameTable)
+  {
+    fprintf(stderr, "Need to use --name-table to specify taxonomy names.\n") ;
+    return EXIT_FAILURE ;
+  }
+  if (!conversionTable)
+  {
+    fprintf(stderr, "Need to use --conversion-table to specify sequence id to taxonomy id mapping.\n") ;
+    return EXIT_FAILURE ;
+  }
+
+  taxonomy.Init(taxonomyFile, nameTable, conversionTable) ;
+  //FixedSizeElemArray genome ;
   //fmBuilder.Build(genome, genomeLength,)
   
+  FILE *fpOutput ;
+  sprintf(outputFileName, "%s.2.cfr", outputPrefix) ;
+  fpOutput = fopen(outputFileName, "w") ;
+  taxonomy.Save(fpOutput) ;
+  fclose(fpOutput) ;
+  
+  free(taxonomyFile) ;
+  free(nameTable) ;
+  free(conversionTable) ;
+
   return 0 ;
 }
