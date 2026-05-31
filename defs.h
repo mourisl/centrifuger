1024100c8abe05109bb40c3036a238654c1597a6
mourisl <mourisl@hotmail.com>
2026-05-30 23:47:33 -0400

    Bump up version number

--- defs.h
+++ defs.h
@@ -5,7 +5,7 @@
 
 //#define DEBUG
 
-#define CENTRIFUGER_VERSION "1.1.2-r325"
+#define CENTRIFUGER_VERSION "1.1.2-r328"
 
 #define SAMPLE_SHEET_SEPARATOR_READ_ID "__centrifuger_sample_sheet_separator__"
 
