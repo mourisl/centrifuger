eb5a3486d69b7c314214dd8a92e1d868353bd0c7
mourisl <mourisl@hotmail.com>
2026-08-04 11:15:11 -0400

    change dl=0 to dl=1 just in case

--- defs.h
+++ defs.h
@@ -5,7 +5,7 @@
 
 //#define DEBUG
 
-#define CENTRIFUGER_VERSION "1.1.3-r331"
+#define CENTRIFUGER_VERSION "1.1.3-r332"
 
 #define SAMPLE_SHEET_SEPARATOR_READ_ID "__centrifuger_sample_sheet_separator__"
 
