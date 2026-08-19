8943585a1f1eb6c0d09984c055dbaf3d7f578d4b
mourisl <mourisl@hotmail.com>
2026-08-19 13:59:10 -0400

    Update version number

--- defs.h
+++ defs.h
@@ -5,7 +5,7 @@
 
 //#define DEBUG
 
-#define CENTRIFUGER_VERSION "1.1.3-r343"
+#define CENTRIFUGER_VERSION "1.1.3-r344"
 
 #define SAMPLE_SHEET_SEPARATOR_READ_ID "__centrifuger_sample_sheet_separator__"
 
